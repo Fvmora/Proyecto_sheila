bb7f771e56a4af5e0019f6876bfe50601822361e
Fvmora <fernandovelascomora@gmail.com>
2018-03-15 09:22:43 +0100

    Cambios en el menu de navegación principal 15_03_2018

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function (e) {
+
+    var on = $('#button_equipo');
+    var off = $('#desp_equip');
+    var on1 = $('#button_coach');
+    var off1 = $('#desp_coach');
+
+
+    on.click(function (e) {
+
+        off.slideToggle();
+
+
+    })
+    on1.click(function (e) {
+
+       off1.slideToggle();
+
+
+    })
+    
+
+    
+    
+    
+
+
+})
+
+
+
